e2bc8076cf7b33e64197644658f49b67c1fdcd41
tobias.stelzer@yahoo.com <42136023+TStelzer86@users.noreply.github.com>
2020-10-07 11:02:28 +0200

    Added DataPreparation
    Data preparation to create balanced subsets of the unbalanced dataset raw.

---
+++ DataPreparation/Data_Preparation.py
@@ -0,0 +1,52 @@
+import os
+import numpy as np
+import shutil
+
+# Definitions
+dir_name = os.path.dirname(__file__)
+subfolder = 'segmented'
+image_dir = os.path.join(dir_name,'raw',subfolder)
+train_target_dir = os.path.join(dir_name,'data','train',subfolder)
+test_target_dir = os.path.join(dir_name,'data','test',subfolder)
+# only needed if train data is not split into train and val in training script
+#val_target_dir = os.path.join(dir_name,'data','val',subfolder)
+
+n = []
+
+min_images = 1499
+
+if not os.path.exists('data'):
+    os.makedirs('data')
+if not os.path.exists('data/train'):
+    os.makedirs('data/train')
+if not os.path.exists('data/test'):
+    os.makedirs('data/test')
+#if not os.path.exists('data/val'):
+#    os.makedirs('data/val')
+
+for folder in os.listdir(image_dir):
+    print(folder)
+    image_list = os.listdir(os.path.join(image_dir,folder))
+    print(len(image_list))
+    n.append(len(image_list))
+
+    if len(image_list) > min_images:
+        if not os.path.exists(os.path.join(train_target_dir,folder)):
+            os.makedirs(os.path.join(train_target_dir,folder))
+        if len(os.listdir(os.path.join(train_target_dir,folder))) == 0:
+            for image in image_list[((min_images+1)/6):]:
+                shutil.copy(os.path.join(image_dir,folder,image),os.path.join(train_target_dir,folder,image))
+
+        if not os.path.exists(os.path.join(test_target_dir,folder)):
+            os.makedirs(os.path.join(test_target_dir,folder))
+        if len(os.listdir(os.path.join(test_target_dir,folder))) == 0:
+            for image in image_list[:((min_images+1)/6)]:
+                shutil.copy(os.path.join(image_dir,folder,image),os.path.join(test_target_dir,folder,image))
+
+    #if not os.path.exists(os.path.join(val_target_dir,folder)):
+    #    os.makedirs(os.path.join(val_target_dir,folder))
+    #if len(os.listdir(os.path.join(val_target_dir,folder))) == 0:
+    #    for image in image_list[0:20]:
+    #        shutil.copy(os.path.join(image_dir,folder,image),os.path.join(val_target_dir,folder,image))
+
+print("Minimal data of %d images" % n[np.argmin(n)])
--- README.md
+++ README.md
@@ -6,3 +6,5 @@ The first model is based on this blog post: https://towardsdatascience.com/plant
 Added second model for experiments and comparison. Switch by changing mdl in SDGModelTrainer.py.
 
 Saved model and data are version controlled by DVC.
+
+The images in DataPreaparation\raw form an inbalanced dataset, meaning there are different amounts of images per class. The script Data_Preaparation.py creates a balanced data subset with only those classes, that contain at least (min_images+1).
